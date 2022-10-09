bbbcd52f2f7884bfdb852e448c0b141683a447fc
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2022-10-09 13:37:24 +0300

    Completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {};
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+  employee = { ...employee, [key]: value };
+  return employee;
+};
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+  employee[key] = value;
+  return employee;
+};
+
+const deleteFromEmployeeByKey = (employee, key) => {
+  employee = { ...employee, key };
+  delete employee[key];
+  return employee;
+};
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+  delete employee[key];
+  return employee;
+}
